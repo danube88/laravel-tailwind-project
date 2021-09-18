e19cad3e3b7dd55a66365708d07a98e89722e674
Alexander <36491066+danube88@users.noreply.github.com>
2021-09-19 00:29:14 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,17 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <title></title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
     </head>
     <body>
-
+        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+            <div class="flex-shrink-0">
+                <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+            </div>
+            <div>
+                <div class="text-xl font-medium text-black">ChitChat</div>
+                <p class="text-gray-500">У вас новое сообщение!</p>
+            </div>
+        </div>
     </body>
 </html>
